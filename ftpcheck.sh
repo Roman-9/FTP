c3ffb79797eec8600ff612ed6aa1b229764cc119
Mihnea :) <badiumihnea16@gmail.com>
2026-01-02 21:20:41 +0200

    Test pentru merge error

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -4,5 +4,6 @@
 #Daca nu afisez cu echo eroare, Dc da il salvez intr-o variabila
 
 #Verificam daca exista fisierul pe care l-am mentionat ca argument
-
+#linie de test 1
+#linie de test 2
 #Verificam permisiunile si proprietarul fisierului
