7f37ba85da7ff2f4942e9827b83d253c3bddf3f0
Mihnea :) <badiumihnea16@gmail.com>
2026-01-02 23:14:00 +0200

    Am verificat permisiunile si ownership-ul V2

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -3,9 +3,20 @@
 #verificam daca exista argument cand apelez script-ul
 #Daca nu afisez cu echo eroare, Dc da il salvez intr-o variabila
 
-#verificam daca ne da eroare la merge
+if [ "$#" -ne 1 ]; then
+	echo "Eroare! Trebuie specificata calea catre fisierul de configurare."
+	echo "Sintaxa corecta: $0 /.../vsftpd.conf"
+	exit 1
+fi
+
+FILE="$1"
 
 #Verificam daca exista fisierul pe care l-am mentionat ca argument
+
+if [ ! -f "$FILE" ]; then
+	echo "Eroare! Fisierul '$FILE' nu exista"
+	exit 1
+fi
 
 #Verificam permisiunile si proprietarul fisierului
 
