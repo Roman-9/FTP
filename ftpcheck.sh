b2a37b10ddf7b0c17378553492e96e595d9952f7
Roman Andrei <andrei2006roman@gmail.com>
2026-01-02 21:15:37 +0200

    Am facut planul partial

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "Testam..."
-echo "Test merge"
+
+#verificam daca exista argument cand apelez script-ul
+#Daca nu afisez cu echo eroare, Dc da il salvez intr-o variabila
+
+#Verificam daca exista fisierul pe care l-am mentionat ca argument
+
+#Verificam permisiunile si proprietarul fisierului
